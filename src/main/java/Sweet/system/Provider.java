a212f4bf07f533b5010831355a878a3458a466a3
abdelrahman962 <113623023+abdelrahman962@users.noreply.github.com>
2024-08-25 13:56:11 +0300

    Update Provider.java

--- src/main/java/Sweet/system/Provider.java
+++ src/main/java/Sweet/system/Provider.java
@@ -1,109 +1,17 @@
 package Sweet.system;
 
+public class Provider extends ProductOwner {
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
-public class Provider {
-    private String email;
-    private String password;
-    private String city;
-    public List<Product> products;
-
-    public Provider(String email, String password , String city) {
-        this.email = email;
-        this.password = password;
-        this.city = city;
-        this.products = new ArrayList<>();
-
+    public Provider(String email, String password, String city) {
+        super(email, password, city);
     }
 
-
+    @Override
     public void addProduct(Product product) {
+        
         product.setproviderEmail(this.email);
-        products.add(product);
+        super.addProduct(product);
     }
 
-
-
-    public void removeProduct(Product product) {
-        products.remove(product);
-    }
-
-    public void updateProduct(Product updatedProduct) {
-        Product existingProduct = findProductByName(updatedProduct.getName());
-        if (existingProduct != null) {
-            existingProduct.setName(updatedProduct.getName());
-            existingProduct.setPrice(updatedProduct.getPrice());
-            existingProduct.setDescription(updatedProduct.getDescription());
-            existingProduct.setDietaryNeeds(updatedProduct.getDietaryNeeds());
-        }
-    }
-
-
-
-
-
-
-    public List<Product> getProducts() {
-        return new ArrayList<>(products);
-    }
-    public String getCity() {
-        return city;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setEmail(String newEmail) {
-        this.email=newEmail;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public List<Message> viewMessages(Login login) {
-        return login.getMessagesForStoreOwner(email);
-    }
-
-    public void respondToMessage(Login login, String userEmail, String response) {
-        login.sendMessageToStoreOwner(email, userEmail, response);
-    }
-
-    public Product findProductByName(String productName) {
-        for (Product product : products) {
-            if (product.getName().equals(productName)) {
-                return product;
-            }
-        }
-        return null;
-
-    }
-
-    public double getTotalSales() {
-        return products.stream().mapToDouble(Product::getSales).sum();
-    }
-
-    public double getTotalRevenue() {
-        return products.stream().mapToDouble(Product::getTotalRevenue).sum();
-    }
-
-    public List<Product> getBestSellingProducts() {
-        List<Product> sortedProducts = new ArrayList<>(products);
-        sortedProducts.sort(Comparator.comparingDouble(Product::getSales).reversed());
-        return sortedProducts;
-    }
-
-
-
+    
 }
